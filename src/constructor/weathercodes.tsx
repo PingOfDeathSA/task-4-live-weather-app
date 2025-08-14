7dd906fcb932bbe77d7096c982c5529267f87a85
RonaldSiyangoma <ronaldnt8@gmail.com>
2025-08-14 11:39:29 +0200

    Fix images paths

--- src/constructor/weathercodes.tsx
+++ src/constructor/weathercodes.tsx
@@ -2,18 +2,22 @@ export class WeatherCodes {
     constructor(){
         console.log("WeatherCodes");
     }
-    static getWetherCode(){
-        const weatherMapping: Record<number, { text: string; icon: string }> = {
-  0: { text: "Clear", icon: "../images/Sun.png" },
-  1: { text: "Mainly clear", icon: "../images/Clear Sky.png" },
-  2: { text: "Partly cloudy", icon: "../images/Partly Cloudy.png" },
-  3: { text: "Overcast", icon: "../images/Rain Cloud.png" },
-  61: { text: "Rain", icon: "../images/Rain.png" },
-  63: { text: "Rain showers", icon: "../images/Rain.png" },
-  80: { text: "Showers", icon: "../images/Rain Cloud.png" },
-  95: { text: "Thunderstorm", icon: "../images/Stormy Weather.png" },
-};
+  static getWetherCode() {
+      const weatherMapping: Record<number, { text: string; icon: string }> = {
+        0: { text: "Clear", icon: `${process.env.PUBLIC_URL}/images/Sun.png` },
+        1: { text: "Mainly clear", icon: `${process.env.PUBLIC_URL}/images/Clear Sky.png` },
+        2: { text: "Partly cloudy", icon: `${process.env.PUBLIC_URL}/images/Partly Cloudy.png` },
+        3: { text: "Overcast", icon: `${process.env.PUBLIC_URL}/images/Rain Cloud.png` },
+        61: { text: "Rain", icon: `${process.env.PUBLIC_URL}/images/Rain.png` },
+        63: { text: "Rain showers", icon: `${process.env.PUBLIC_URL}/images/Rain.png` },
+        80: { text: "Showers", icon: `${process.env.PUBLIC_URL}/images/Rain Cloud.png` },
+        95: { text: "Thunderstorm", icon: `${process.env.PUBLIC_URL}/images/Stormy Weather.png` },
+      };
 
-return weatherMapping
-    }
+      return weatherMapping;
+  }
+
 }
+
+
+
